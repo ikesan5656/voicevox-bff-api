f3d73bed32d5c5f6a99a38b9d684b93a6cdc3e5c
池ノ谷優太 <you1207.ike@gmail.com>
2025-03-27 22:35:08 +0900

    #2：codegenie/serverless-expressをlambdaで実行できるように

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { useExpressServer } from "routing-controllers";
 import { UserController } from "./controllers/userController";
 import "reflect-metadata";
+
 const app = express();
 const port = 3000;
 
