f6410ab7eb7ade017f9aedde137efefd28ffde46
池ノ谷優太 <you1207.ike@gmail.com>
2026-01-19 20:50:43 +0900

    orval設定変更、openapi.yaml再生成、openapi関連のスクリプト追加

---
+++ app/orval.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  bff: {
+    input: {
+      target: "src/openapi/bff/spec/openapi.yaml",
+    },
+    output: {
+      mode: "tags",
+      target: "src/api/bff/endpoints",
+      schemas: "src/api/bff/models",
+      clean: true,
+      client: "axios",
+    },
+  },
+  /*voicevox: {
+    input: {
+      target: "src/openapi/voicevox/spec/openapi.yaml",
+    },
+    output: {
+      mode: "tags",
+      target: "src/api/voicevox/endpoints",
+      schemas: "src/api/voicevox/models",
+      clean: true,
+      client: "axios",
+    },
+  },*/
+  voicevox_synthesis: {
+    input: {
+      target: "src/openapi/voicevox/openapi.formatted.yaml",
+      filters: {
+        tags: ["speech_synthesis"],
+      },
+    },
+    output: {
+      mode: "tags",
+      target: "src/api/voicevox/synthesis/endpoints",
+      schemas: "src/api/voicevox/models",
+      clean: true,
+      client: "axios-functions", // axiosだとファクトリ関数が作られてしまう
+      override: {},
+    },
+  },
+});
